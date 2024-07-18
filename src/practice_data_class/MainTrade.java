e71f99b2b9ccd670892b54e53d531a1fee8be8e7
eastorigin <eastorigin21@gmail.com>
2024-07-18 23:16:24 +0900

    숙제 완료

--- src/practice_data_class/MainTrade.java
+++ src/practice_data_class/MainTrade.java
@@ -3,12 +3,24 @@ package practice_data_class;
 public class MainTrade {
 	public static void main (String[] args) {
 		
-		Seller seller = new Seller(2, 230, 100000);
+		Seller seller = new Seller(5, 1000, 100000);
 		Purchaser purchaser = new Purchaser(0, 8000);
 		
 		Trade trade = new Trade(seller, purchaser);
 		
-		trade.purchase(5);
-		trade.purchase(100);
+		System.out.println("정상적으로 거래가 이루어지는 경우");
+		trade.purchase(4);
+		System.out.println("");
+		
+		System.out.println("재고보다 많은 상품을 구매하려 할 경우");
+		trade.purchase(7);
+		System.out.println("");
+		
+		System.out.println("예산이 부족한 경우");
+		seller = new Seller(6, 3000, 100000);
+		purchaser = new Purchaser(0, 8000);
+		trade = new Trade(seller, purchaser);
+		trade.purchase(3);
+		
 	}
 }
--- src/practice_data_class/Trade.java
+++ src/practice_data_class/Trade.java
@@ -11,31 +11,30 @@ public class Trade {
 	}
 	
 	public void purchase (int tradeGoodsNumber) {
-		if(this.purchaser.purchaserGoodsNumber > this.seller.sellerGoodsNumber) {
-			this.seller.sellerMoney += this.seller.goodsPrice * this.seller.sellerGoodsNumber;
-			this.purchaser.purchaserGoodsNumber += this.seller.sellerGoodsNumber;
-			this.purchaser.purchaserBudget -= this.seller.goodsPrice * this.seller.sellerGoodsNumber;
-			this.seller.sellerGoodsNumber = 0;
-			System.out.println("구매자의 상품 수: " + this.purchaser.purchaserGoodsNumber);
-			System.out.println("구매자의 지갑 현황: " + this.purchaser.purchaserBudget + "원");
-			System.out.println("");
-			System.out.println("판매자의 상품 수: " + this.seller.sellerGoodsNumber);
-			System.out.println("판매자의 상품 가격: " + this.seller.goodsPrice + "원");
-			System.out.println("판매자의 금고 현황: " + this.seller.sellerMoney + "원");
-		}else if(this.purchaser.purchaserBudget < this.seller.goodsPrice * tradeGoodsNumber) {
-			System.out.println("거래가 성립되지 않아 아무런 변화가 없다");
-		}else {
-			this.purchaser.purchaserGoodsNumber += tradeGoodsNumber;
-			this.seller.sellerGoodsNumber -= tradeGoodsNumber;
-			this.purchaser.purchaserBudget -= this.seller.goodsPrice * tradeGoodsNumber;
-			this.seller.sellerMoney += this.seller.goodsPrice * tradeGoodsNumber;
-			System.out.println("구매자의 상품 수: " + this.purchaser.purchaserGoodsNumber + "개");
-			System.out.println("구매자의 지갑 현황: " + this.purchaser.purchaserBudget + "원");
-			System.out.println("");
-			System.out.println("판매자의 상품 수: " + this.seller.sellerGoodsNumber + "개");
-			System.out.println("판매자의 상품 가격: " + this.seller.goodsPrice + "원");
-			System.out.println("판매자의 금고 현황: " + this.seller.sellerMoney + "원");
-
+		if(tradeGoodsNumber > this.seller.sellerGoodsNumber) {
+			tradeGoodsNumber = this.seller.sellerGoodsNumber;
 		}
+		
+		if(this.purchaser.purchaserBudget < this.seller.goodsPrice * tradeGoodsNumber) {
+			System.out.println("거래가 성립되지 않아 아무런 변화가 없다");
+			resultTrade();
+			return;
+		}
+		
+		this.purchaser.purchaserGoodsNumber += tradeGoodsNumber;
+		this.seller.sellerGoodsNumber -= tradeGoodsNumber;
+		this.purchaser.purchaserBudget -= this.seller.goodsPrice * tradeGoodsNumber;
+		this.seller.sellerMoney += this.seller.goodsPrice * tradeGoodsNumber;
+			
+		resultTrade();
+	}
+	
+	public void resultTrade() {
+		System.out.println("구매자의 상품 수: " + this.purchaser.purchaserGoodsNumber + "개");
+		System.out.println("구매자의 지갑 현황: " + this.purchaser.purchaserBudget + "원");
+		System.out.println("");
+		System.out.println("판매자의 상품 수: " + this.seller.sellerGoodsNumber + "개");
+		System.out.println("판매자의 상품 가격: " + this.seller.goodsPrice + "원");
+		System.out.println("판매자의 금고 현황: " + this.seller.sellerMoney + "원");
 	}
 }
